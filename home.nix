a9e0730892bf0e54b6ba41934b963acf6a3da2da
max@nixos <maxwell-gao314@outlook.com>
2025-12-02 14:13:31 +0000

    add gdk:

--- home.nix
+++ home.nix
@@ -1,4 +1,10 @@
 { config, pkgs, inputs, ... }: 
+let
+  # Define the custom gcloud package set using a 'let' block
+  gdk = pkgs.google-cloud-sdk.withExtraComponents( with pkgs.google-cloud-sdk.components; [
+    gke-gcloud-auth-plugin
+  ]);
+in
 {   
   home.username = "nixos";
   home.stateVersion = "25.05";
@@ -6,6 +12,7 @@
   home.packages = with pkgs; [
     texlive.combined.scheme-full
     pandoc
+    gdk
   ];
   # Zsh configuration
   programs.zsh = {
